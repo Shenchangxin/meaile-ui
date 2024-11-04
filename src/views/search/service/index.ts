3a410c47a8492789ebea84c873a553148899e6c7
Shenchangxin <2863727114@qq.com>
2024-11-04 22:54:18 +0800

    开发搜索页面

--- src/views/search/service/index.ts
+++ src/views/search/service/index.ts
@@ -1,16 +1,30 @@
-import {ref, Ref} from "vue";
+import {ref, Ref, watchEffect} from "vue";
 import {Tag,store as tagStore} from "@/store/modules/tag.ts";
 
 
 
 
-class SearchService{
+export  default  class SearchService{
     static firstTagActiveIndex:Ref<number>=ref(0)
 
     static tagList: Ref<Tag[]> = ref([])
 
+    static secondTagList: Ref<Tag[]> = ref([])
+
     //查询标签数据
-    static async getTagList(){
+    static async getFirstTagList(){
         tagStore.actions.getTagList()
+        SearchService.tagList.value = tagStore.getters.getTagList
     }
+    static changeTab(index: number){
+        SearchService.firstTagActiveIndex.value=index;
+    }
+
+    static getSecondTagList(){
+        watchEffect(async () => {
+            await tagStore.actions.getTagList(SearchService.firstTagActiveIndex.value+1)
+            SearchService.secondTagList.value = tagStore.getters.getTagList
+        })
+    }
+
 }
