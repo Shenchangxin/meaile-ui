ad1790050b53fc52ecc257f1c3b20f09810a9bf6
Shenchangxin <2863727114@qq.com>
2024-12-16 17:57:01 +0800

    调整样式布局

--- src/views/foodbook/components/toSearch.vue
+++ src/views/foodbook/components/toSearch.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="content">
-    <i class="iconfont icon-xiangzuojiantou" @click="back()"></i>
+    <i class="iconfont icon-xiangzuojiantou xiangzuojiantou" @click="back()"></i>
     <div class="tosearch">
       <div class="tosearch-keyword">
         <i class="iconfont icon-fangdajing fangdajing"></i>
         <div class="tosearch-keyword">
-          <input type="text" class="tosearch-keyword-input"/>
-          <span class="keyword">{{getSecondTag.tagName}}</span>
+          <input type="text" v-model="secondTagName" class="tosearch-keyword-input" />
+          <!--          <span class="keyword">{{getSecondTag.tagName}}</span>-->
           <i class="iconfont icon-close"></i>
         </div>
       </div>
@@ -16,29 +16,50 @@
 </template>
 
 <script setup lang="ts">
-import SearchService from "@/views/search/service";
-const {storeRefs,back} = SearchService
+import SearchService from "@/views/search/service"
+import { onMounted, ref } from "vue"
 
-const {getSecondTag} = storeRefs
+const { storeRefs, back } = SearchService
 
+const { getSecondTag } = storeRefs
+// 创建一个响应式引用来存储从 getSecondTag 函数获取的标签值
+const secondTagName = ref<string>("")
+
+// 在组件挂载时调用 getSecondTag 函数，并设置 tagName 的值
+onMounted(() => {
+  // 假设 getSecondTag 返回一个对象，该对象有一个 tagName 属性
+  const secondTagData = getSecondTag
+  console.log("secondTagData:" + secondTagData.value.tagName)
+  if (secondTagData.value && secondTagData.value.tagName) {
+    secondTagName.value = secondTagData.value.tagName
+  }
+})
 
 
 </script>
 
 <style lang="scss" scoped>
 @use "../../../assets/css/common";
-.content{
+
+.content {
   @extend .multiplex-content;
   z-index: 2;
-  .tosearch{
+
+  .tosearch {
     @extend .multiplex-tosearch;
-    .fangdajing{
-      fonts-size: 0.36rem;
-      margin: 0rem 0.26rem;
+    .fangdajing {
+      margin: 0 0.20rem;
     }
-    &-keyword{
-      display: grid;
-      grid-template-columns: 3rem;
+    &-keyword {
+      display: flex;
+      align-items: center;
+      font-size: 0.24rem;
+      font-weight: 100;
+      .tosearch-keyword-input{
+        height: 0.24rem;
+        background-color: transparent;
+        border: none;
+      }
     }
   }
 }
